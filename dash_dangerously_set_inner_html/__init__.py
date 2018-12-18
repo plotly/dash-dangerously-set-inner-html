db51847fac371dd0adb6390093b699a269e7d01b
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-12-18 17:38:35 -0500

    fix react 16, bump version, more flexible build artefacts

--- dash_dangerously_set_inner_html/__init__.py
+++ dash_dangerously_set_inner_html/__init__.py
@@ -1,6 +1,7 @@
 import os as _os
 import dash as _dash
 import sys as _sys
+import json
 from .version import __version__
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
@@ -12,15 +13,22 @@ _components = _dash.development.component_loader.load_components(
 
 _this_module = _sys.modules[__name__]
 
+_basepath = _os.path.dirname(__file__)
+_filepath = _os.path.abspath(_os.path.join(_basepath, 'package.json'))
+with open(_filepath) as f:
+    package = json.load(f)
+
+js_package_name = package['name']
+py_package_name = __name__
+js_version = package['version']
 
 _js_dist = [
     {
         'relative_package_path': 'bundle.js',
         'external_url': (
-            'https://unpkg.com/dash-dangerously-set-inner-html@{}'
-            '/dash_dangerously_set_inner_html/bundle.js'
-        ).format(__version__),
-        'namespace': 'dash_dangerously_set_inner_html'
+            'https://unpkg.com/{}@{}/{}/bundle.js'
+        ).format(js_package_name, js_version, py_package_name),
+        'namespace': py_package_name
     }
 ]
 
